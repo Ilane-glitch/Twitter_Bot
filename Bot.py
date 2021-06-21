8f3cc59e42c3eeac3a9e96f7a7d339eb85f5bf03
Ilane <76136529+Ilane-glitch@users.noreply.github.com>
2021-06-21 14:42:28 +0200

    update

--- Bot.py
+++ Bot.py
@@ -1,2 +1,23 @@
-#ceci est un test
+#!/usr/bin/env python
+
+__author__ = "Ilane"
+__version__ = "0.1"
+
+#import
 import tweepy
+
+# Variables
+CONSUMER_KEY = ''
+CONSUMER_SECRET = ''
+ACCESS_TOKEN =''
+ACCESS_TOKEN_SECRET = ''
+
+# Authenticate to Twitter
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+
+# Create API object
+api = tweepy.API(auth)
+
+# Create a tweet
+api.update_status("Hello Tweepy")
